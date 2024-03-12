452e8b3c96e9490663140e023aaf7625531f5591
“GanapathyPonraj” <ganapathy.ponraj96@gmail.com>
2024-03-12 01:54:44 -0400

    apiEndpoint changed

--- src/features/weatherApp/reduxStore/weatherAPI.js
+++ src/features/weatherApp/reduxStore/weatherAPI.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export function todayApiCall1(cityName){
     const responseFinal = 
-    axios.get(`http://api.weatherapi.com/v1/forecast.json?key=360f9c9c27f44b5284320253221812&q=${cityName}&days=7&aqi=no&alerts=no`)
+    axios.get(`https://api.weatherapi.com/v1/forecast.json?key=360f9c9c27f44b5284320253221812&q=${cityName}&days=7&aqi=no&alerts=no`)
     .then(response => {
         return response.data
     })
